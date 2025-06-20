408c6b51691e0decb757e0d456c219095f139681
Ngo Xuan Nghia <JohnNgo1311@gmail.com>
2025-06-20 16:49:11 +0700

    Fix time load, fix UI, fix GrapperRepository nad Presenter

--- TTC_AR/Assets/Scripts/Presentation/Presenters/LoginPresenter.cs
+++ TTC_AR/Assets/Scripts/Presentation/Presenters/LoginPresenter.cs
@@ -76,7 +76,7 @@ public class LoginPresenter
 
         try
         {
-            // var companyTask = _companyService.GetCompanyByIdAsync(1);
+            var companyTask = _companyService.GetCompanyByIdAsync(1);
             var grapperTask = _grapperService.GetListGrapperAsync(1);
             var jbTask = _jbService.GetListJBInformationAsync(1);
             var imageTask = _imageService.GetListImageAsync(1);
@@ -88,13 +88,14 @@ public class LoginPresenter
             var adapterSpecificationTask = _adapterSpecificationService.GetListAdapterSpecificationAsync(1);
             var rackTask = _rackService.GetListRackAsync(1);
 
+
             await Task.WhenAll(
-                // companyTask,
+                companyTask,
                 grapperTask, jbTask, imageTask, moduleTask, mccTask, fieldDeviceTask, deviceTask,
                  moduleSpecificationTask, adapterSpecificationTask,
                  rackTask);
 
-            // var companyDto = companyTask.Result;
+            var companyDto = companyTask.Result;
             var grapperDtos = grapperTask.Result;
             var jbDtos = jbTask.Result;
             var imageDtos = imageTask.Result;
@@ -107,7 +108,7 @@ public class LoginPresenter
             var rackDtos = rackTask.Result;
 
             if (
-            // companyDto == null||
+            companyDto == null ||
             grapperDtos == null
             || jbDtos == null || imageDtos == null
             || moduleDtos == null
@@ -122,18 +123,18 @@ public class LoginPresenter
                 return;
             }
 
-            // if (companyDto != null)
-            // {
-            //     var models = ConvertCompanyModelFromDto(companyDto);
-            //     if (models != null)
-            //     {
-            //         GlobalVariable.temp_CompanyInformationModel = models;
-            //     }
-            // }
-            // else
-            // {
-            //     _view.ShowError("Tải dữ liệu thất bại!");
-            // }
+            if (companyDto != null)
+            {
+                var models = ConvertCompanyModelFromDto(companyDto);
+                if (models != null)
+                {
+                    GlobalVariable.temp_CompanyInformationModel = models;
+                }
+            }
+            else
+            {
+                _view.ShowError("Tải dữ liệu thất bại!");
+            }
             if (grapperDtos != null)
             {
                 var models = new List<GrapperInformationModel>();
--- TTC_AR/Assets/Scripts/Presentation/Views/AdapterSpecification/Views/UpdateAdapterSpecificationSettingView.cs
+++ TTC_AR/Assets/Scripts/Presentation/Views/AdapterSpecification/Views/UpdateAdapterSpecificationSettingView.cs
@@ -131,7 +131,7 @@ public class UpdateAdapterSpecificationSettingView : MonoBehaviour, IAdapterSpec
         var backButton = DialogOneButton.transform.Find("Background/Back_Button").GetComponent<Button>();
         backButton.gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("images/UIimages/Success_Back_Button_Background");
         var dialog_Icon = DialogOneButton.transform.Find("Background/Dialog_Status_Icon").GetComponent<Image>().sprite = Resources.Load<Sprite>("images/UIimages/Success_Icon_For_Dialog");
-        var dialog_Content = DialogOneButton.transform.Find("Background/Dialog_Content").GetComponent<TMP_Text>().text = $"Bạn đã thành công cập nhật loại Adapter <b><color =#004C8A>{model.Code}</b></color>"; ;
+        var dialog_Content = DialogOneButton.transform.Find("Background/Dialog_Content").GetComponent<TMP_Text>().text = $"Bạn đã thành công cập nhật loại Adapter <b><color = #004C8A>{model.Code}</b></color>"; ;
         var dialog_Title = DialogOneButton.transform.Find("Background/Dialog_Title").GetComponent<TMP_Text>().text = "Cập nhật loại Adapter thành công";
         backButton.onClick.RemoveAllListeners();
         backButton.onClick.AddListener(() =>
